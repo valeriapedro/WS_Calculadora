38e1a5b99aa787a839901315d5159e3f607a268e
Valeria Pedro <valeriatads@gmail.com>
2012-11-18 22:19:16 -0200

    Primeiro commit

---
+++ src/operacoes/BasicOperations.java
@@ -0,0 +1,21 @@
+package operacoes;
+
+public class BasicOperations {
+
+	public int soma (int a, int b){
+		return a + b;
+	}
+	
+	public int subtrai (int a, int b){
+		return a - b;
+	}
+	
+	public int mult (int a, int b){
+		return a * b;		
+	}
+	
+	public int div (int a, int b){
+		return a / b;
+	}
+
+}
